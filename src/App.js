50be3052f2702c07be41ce4c3478c542577213e0
ALEJANDRO <jalejandrob95@gmail.com>
2023-05-23 20:23:01 -0500

    [IMP] createFetching and table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useFetch } from './useFetch';
+import { Table } from 'react-bootstrap';
 
 function App() {
+  const { data, loading, error } = useFetch(
+    'http://localhost:3003/v1/files/data'
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container" style={{ paddingTop: '20px' }}>
+        <header className="App-header">
+          <h1 class="alert alert-danger">File API</h1>
+          {error && <li>Error...</li>}
+          {loading && <li>Loading..</li>}
+          <ul></ul>
+        </header>
+
+        <Table striped bordered hover responsive>
+          <thead>
+            <tr>
+              <th>File Name</th>
+              <th>Text</th>
+              <th>Number</th>
+              <th>Hex</th>
+            </tr>
+          </thead>
+          <tr></tr>
+          <tbody class="table-group-divider">
+            {data?.map((file, fileIndex) =>
+              file.list.map((item, index) => (
+                <tr key={`${fileIndex}-${index}`}>
+                  {index === 0 ? (
+                    <td rowSpan={file.list.length}>{file.file}</td>
+                  ) : null}
+                  <td>{item.text}</td>
+                  <td>{item.number}</td>
+                  <td>{item.hex}</td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </Table>
+      </div>
     </div>
   );
 }
